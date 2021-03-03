c8243a67adaff19d82bc15c5956b8501dea071af
zeromountain <sonyeongsan@gmail.com>
2021-03-03 23:18:54 +0900

    자바스크립트를 타입스크립트처럼 코딩하는 방법
    BUT
    jsDOC을 이용해 타입을 정의하는데 한계가 있다

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,6 +1,15 @@
+// @ts-check
+
+/**
+ * jsDOC
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
 function sum(a, b) {
   return a + b;
 }
-sum(10, "20"); // "1020"
+// ts와 다르게 타입 오류가 발생하지 않는다.
+// ts-check 사용 시 오류가 발생하게 할 수 있음
+sum(10, "20");
 var result = sum(5, 10);
 result.toLocaleString();
