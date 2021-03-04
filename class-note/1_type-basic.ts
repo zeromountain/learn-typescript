e04f7128d3381cafa03a310954aee2f67c8c4c98
zeromountain <sonyeongsan@gmail.com>
2021-03-05 01:06:26 +0900

    기본타입
    타입스크립트에서 배열, 객체, 불린 타입을 변수에 할당하는 방법

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -12,4 +12,25 @@ var arr: Array<number> = [1, 2, 3];
 // 배열 안의 요소의 타입을 정의한 타입으로 입력하지 않으면 오류 발생
 var heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10];
 // 배열 리터럴을 활용한 타입 표현
-var items: number[] = [1, 2, 3] 
+var a: number = 10;
+var items: number[] = [1, 2, 3, a];
+
+//TS 튜플: 배열의 특정 인덱스의 타입을 정의하는 방법
+let address: [string, number] = ['gangnam', 'pangyo'];
+address = ['gangnam', 10];
+
+// TS 객체
+let obj: object = {};
+
+let capt: object = {
+  name: 'capt',
+  age: 100
+}
+
+let thor: {name: string, age: number} = {
+  name: 'thor',
+  age: 1000
+}
+
+// TS 진위값: 불린타입
+let show: boolean = true; 
